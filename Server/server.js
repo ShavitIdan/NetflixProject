1e6609fe3d2732ebe25fffbd4e5e02c012e22701
ShavitIdan <idanshavit5@gmail.com>
2025-04-11 15:48:34 +0300

    init db

---
+++ Server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+require("./config/db");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+    
+app.use(cors());
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
