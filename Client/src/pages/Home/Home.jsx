e0d907be7220106ad8038df62be1d36b30fd387b
edenadiv <info@edenadiv.com>
2025-04-19 22:02:30 +0300

    Home page building

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -2,11 +2,21 @@ import React from 'react'
 import './Home.css'
 import Navbar from '../../compenents/Navbar/Navbar'
 import Footer from '../../compenents/Footer/Footer'
+import hero from '../../assets/cover.png'
+import heroOverlay from '../../assets/cover_hover.png'
+import coverHover from '../../assets/only_on_hover2.png'
 
 const Home = () => {
   return (
     <div className='home'>
       <Navbar/>
+      <div className="hero">
+        <img src={hero} alt="" className='hero-banner' />
+        <div className="hero-overlay">
+          <img src={heroOverlay} alt="" className='caption-img'/>
+          <p>A gripping TENFLIX thriller set in a snowbound town where a deadly storm traps residents—and something sinister begins to emerge. Secrets surface, paranoia spreads, and survival takes a dark turn.</p>
+        </div>
+      </div>
     </div>
   )
 }
