3aa7a73a22df20ae16fc69cdf47618496bccba56
edenadiv <info@edenadiv.com>
2025-04-23 14:58:52 +0300

    Update endpoints

--- Client/src/config/api.js
+++ Client/src/config/api.js
@@ -4,5 +4,11 @@ export const API_ENDPOINTS = {
   AUTH: {
     LOGIN: `${API_BASE_URL}/auth/login`,
     REGISTER: `${API_BASE_URL}/auth/register`,
+  },
+  PROFILE: {
+    GET_PROFILES: `${API_BASE_URL}/profile`,
+    CREATE_PROFILE: `${API_BASE_URL}/profile`,
+    UPDATE_PROFILE: `${API_BASE_URL}/profile/:profileId`,
+    DELETE_PROFILE: `${API_BASE_URL}/profile/:profileId`
   }
 }; 
