d033821a121d67a2be7e32495807a01ff55aeaaf
ShavitIdan <idanshavit5@gmail.com>
2025-04-24 20:46:54 +0300

    localhost errorrs

--- Client/src/services/tmdbService.js
+++ Client/src/services/tmdbService.js
@@ -4,7 +4,7 @@ import { API_BASE_URL } from '../config/api';
 class TMDBService {
   async getPopularMovies(page = 1) {
     try {
-      const response = await axios.get(`${API_BASE_URL}/movies/popular?page=${page}`, {
+      const response = await axios.get(`${API_BASE_URL}/tmdb/movies/popular?page=${page}`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
@@ -18,7 +18,7 @@ class TMDBService {
 
   async getPopularTVShows(page = 1) {
     try {
-      const response = await axios.get(`${API_BASE_URL}/tv/popular?page=${page}`, {
+      const response = await axios.get(`${API_BASE_URL}/tmdb/tv/popular?page=${page}`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
@@ -32,7 +32,7 @@ class TMDBService {
 
   async getNewestContent() {
     try {
-      const response = await axios.get(`${API_BASE_URL}/newest`, {
+      const response = await axios.get(`${API_BASE_URL}/tmdb/newest`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
@@ -46,7 +46,7 @@ class TMDBService {
 
   async getMostViewedInIsrael() {
     try {
-      const response = await axios.get(`${API_BASE_URL}/most-viewed-israel`, {
+      const response = await axios.get(`${API_BASE_URL}/tmdb/most-viewed-israel`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
@@ -60,7 +60,7 @@ class TMDBService {
 
   async getTopRated() {
     try {
-      const response = await axios.get(`${API_BASE_URL}/top-rated`, {
+      const response = await axios.get(`${API_BASE_URL}/tmdb/top-rated`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
@@ -74,7 +74,7 @@ class TMDBService {
 
   async getAnimatedContent() {
     try {
-      const response = await axios.get(`${API_BASE_URL}/animated`, {
+      const response = await axios.get(`${API_BASE_URL}/tmdb/animated`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
@@ -88,7 +88,7 @@ class TMDBService {
 
   async getActionContent() {
     try {
-      const response = await axios.get(`${API_BASE_URL}/action`, {
+      const response = await axios.get(`${API_BASE_URL}/tmdb/action`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
@@ -102,7 +102,7 @@ class TMDBService {
 
   async getMovieDetails(movieId) {
     try {
-      const response = await axios.get(`${API_BASE_URL}/movie/${movieId}`, {
+      const response = await axios.get(`${API_BASE_URL}/tmdb/movie/${movieId}`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
@@ -116,7 +116,7 @@ class TMDBService {
 
   async getTVShowDetails(tvId) {
     try {
-      const response = await axios.get(`${API_BASE_URL}/tv/${tvId}`, {
+      const response = await axios.get(`${API_BASE_URL}/tmdb/tv/${tvId}`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
