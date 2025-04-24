7158f060efe7ba09b27c209d226ccaa919545caa
ShavitIdan <idanshavit5@gmail.com>
2025-04-24 20:41:03 +0300

    changeing local host url

--- Client/src/services/tmdbService.js
+++ Client/src/services/tmdbService.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-
-const API_BASE_URL = 'http://localhost:8080/api/tmdb';
+import { API_BASE_URL } from '../config/api';
 
 class TMDBService {
   async getPopularMovies(page = 1) {
